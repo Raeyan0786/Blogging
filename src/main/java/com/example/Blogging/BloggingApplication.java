4deac2ae6472429f9a89d871e621aaac5d5fc2f3
Raeyan0786 <reyankhan01999@gmail.com>
2023-05-21 23:53:34 +0530

    Update
    Update

--- src/main/java/com/example/Blogging/BloggingApplication.java
+++ src/main/java/com/example/Blogging/BloggingApplication.java
@@ -1,11 +1,12 @@
 package com.example.Blogging;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
+@OpenAPIDefinition
 @SpringBootApplication
 public class BloggingApplication implements CommandLineRunner {
 
